8cdab933c250e2a4b498ef225abcd4b3b442a376
nashika <info@nashika.net>
2016-08-27 20:05:59 +0900

    creating web ui

--- src/common/entity/base-entity.ts
+++ src/common/entity/base-entity.ts
@@ -1,4 +1,14 @@
+import _ = require("lodash");
+
 export abstract class BaseEntity {
+
   _id:string;
   name:string;
+
+  constructor(data:any) {
+    _.forIn(data, (value, key) => {
+      _.set(this, key, value);
+    });
+  }
+
 }
--- src/server/node/server-node.ts
+++ src/server/node/server-node.ts
@@ -4,6 +4,7 @@ import {NotifierNode} from "./notifier/notifier-node";
 import {NotifierGeneratorNode} from "./notifier/notifier-generator-node";
 import {JobNode} from "./job-node";
 import {tableRegistry} from "../table/table-registry";
+import {ServerEntity} from "../../common/entity/server-entity";
 
 export class ServerNode extends BaseNode {
 
@@ -24,7 +25,7 @@ export class ServerNode extends BaseNode {
       if (serverEntity) {
         this.entity = serverEntity;
       } else {
-        this.entity = {name: "SV01"};
+        this.entity = new ServerEntity({name: "SV01"});
         return tableRegistry.server.upsert(this.entity);
       }
     })
