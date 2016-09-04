de824e4694c482ddcc8c6df27d94290e24549ecf
nashika <info@nashika.net>
2016-09-04 12:53:11 +0900

    use socket.io disconnect event

--- src/client/service/socket-io-service.ts
+++ src/client/service/socket-io-service.ts
@@ -22,9 +22,29 @@ export class SocketIoService extends BaseService {
 
   initialize() {
     this.socket = socketIo.connect();
-    this.socket.on("log", this.log);
-    this.socket.on("status", this.status);
+    this.socket.on("disconnect", this.onDisconnect);
+    this.socket.on("log", this.onLog);
+    this.socket.on("status", this.onStatus);
   }
+
+  protected onDisconnect = () => {
+    for (let _id in this.components) {
+      this.components[_id].status = "connecting";
+      this.statuses[_id].status = "connecting";
+    }
+  };
+
+  protected onLog = (data: ISocketIoLogData) => {
+    this.logs.push(data);
+    console.log(`${data._id} [${data.level}] ${data.message}`);
+  };
+
+  protected onStatus = (data: ISocketIoStatusData) => {
+    if (this.components[data._id])
+      this.components[data._id].status = data.status;
+    this.statuses[data._id] = data;
+    console.log(`${data._id} ${data.status}`);
+  };
 
   registerComponent(component: BaseEntityComponent<BaseEntity>) {
     this.components[component.entity._id] = component;
@@ -33,18 +53,6 @@ export class SocketIoService extends BaseService {
   unregisterComponent(_id: string) {
     delete this.components[_id];
   }
-
-  protected log = (data: ISocketIoLogData) => {
-    this.logs.push(data);
-    console.log(`${data._id} [${data.level}] ${data.message}`);
-  };
-
-  protected status = (data: ISocketIoStatusData) => {
-    if (this.components[data._id])
-      this.components[data._id].status = data.status;
-    this.statuses[data._id] = data;
-    console.log(`${data._id} ${data.status}`);
-  };
 
   getStatus(_id: string): TSocketIoStatus {
     return this.statuses[_id] ? this.statuses[_id].status : "connecting";
